3f43020f3a49636c9476e61f488c077e12aa7ff1
script972 <script972@gmail.com>
2017-09-25 00:00:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Model model = new Model();
+        Controller controller = new Controller(model);
+        JFrame window = new JFrame();
+        window.setTitle("2048");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(450, 500);
+        window.setResizable(false);
+
+        window.add(controller.getView());
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
